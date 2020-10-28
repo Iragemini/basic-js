ab46c80967a77bf030fa431c59df25d5557bc922
Ira Karavaichyk <iragemini@gmail.com>
2020-10-28 20:53:21 +0300

    feat: finish carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,29 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let result = "";
+
+  if(typeof sampleActivity !== "string" || !sampleActivity){
+    return false;
+  }
+  if(sampleActivity === "undefined" || sampleActivity === null || sampleActivity === ""){
+    return false;
+  }
+  if(Number.isNaN(Number.parseInt(sampleActivity))){
+    return false;
+  }
+
+  const SAMPLE_ACTIVITY = Number.parseInt(sampleActivity);
+
+  if(SAMPLE_ACTIVITY > MODERN_ACTIVITY || SAMPLE_ACTIVITY <= 0){
+    return false;
+  }
+
+  const k = 0.693/HALF_LIFE_PERIOD;
+
+  const t = Number.parseInt(Math.log(SAMPLE_ACTIVITY/MODERN_ACTIVITY)/k);
+
+  return t;
 };
