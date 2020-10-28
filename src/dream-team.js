addb023c381b2b5d220c40a7d38d7287b6438bac
Ira Karavaichyk <iragemini@gmail.com>
2020-10-28 22:12:44 +0300

    feat: finish dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  //console.log(`members = ${members}`);
+
+  let result = "";
+  let firstLetterArr = [];
+
+  if(Array.isArray(members) === false || members.length === 0){
+    return false;
+  }
+
+  const filterMembers = members.filter(member => typeof member === 'string');
+  if(filterMembers.length === 0){
+    return false;
+  }
+
+  filterMembers.forEach(element => {
+    let firstLetter = element.trim()[0].toUpperCase();
+    firstLetterArr.push(firstLetter);
+  });
+  firstLetterArr.sort();
+  for(let i = 0; i < firstLetterArr.length; i ++){
+    result += firstLetterArr[i];
+  }  
+  return result;
+
 };
