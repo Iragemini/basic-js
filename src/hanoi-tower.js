6aae945113ed1ee60503e4de993f9a8dba0c02f6
Ira Karavaichyk <iragemini@gmail.com>
2020-11-01 12:32:52 +0300

    feat: finish hanoi tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+
+  /* from Wikipedia: минимальное число ходов, 
+  необходимое для решения головоломки, 
+  равно (2 в степени n) − 1, где n — число дисков */
+
+  const turns = Math.pow(2, disksNumber) -1;
+  const turnsSpeedInSec = turnsSpeed / 3600;
+  const seconds = turns / turnsSpeedInSec;
+
+  return {'turns' : turns, 'seconds' : Math.floor(seconds)}
 };
